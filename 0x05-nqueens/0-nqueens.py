01fb891a151ccd18c4974e48e86c5e891171fde1
Obi Chukwu <kennetho.onu@gmail.com>
2024-11-08 03:55:30 +0100

    n-queens puzzle algorithm

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,57 +3,56 @@
 
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    exit(1)
 
-try:
-    N = int(sys.argv[1])
-except ValueError:
-    print("N must be a number")
-    exit(1)
-
-if N < 4:
-    print("N must be at least 4")
-    exit(1)
-
-columns = set()
-pos_diagonal = set()
-neg_diagonal = set()
-board = []
-
-# create the board positions
-for _ in range(N):
-    row = [0] * 2
-    board.append(row)
+def solve(row, column):
+    solver = [[]]
+    for q in range(row):
+        solver = place_queen(q, column, solver)
+    return solver
 
 
-def is_safe(r, c):
-    """Check if a queen can be placed at position (r, c)"""
-    if c in columns or (r + c) in pos_diagonal or (r - c) in neg_diagonal:
-        return False
-    return True
+def place_queen(q, column, prev_solver):
+    solver_queen = []
+    for array in prev_solver:
+        for x in range(column):
+            if is_safe(q, x, array):
+                solver_queen.append(array + [x])
+    return solver_queen
 
 
-def solve_n_queens(r):
-    """recursively places N queens on an N x N chessboard"""
-    if r == N:
-        print(board)
-        return
+def is_safe(q, x, array):
+    if x in array:
+        return (False)
+    else:
+        return all(abs(array[column] - x) != q - column
+                   for column in range(q))
 
-    for c in range(N):
-        if is_safe(r, c):
-            columns.add(c)
-            pos_diagonal.add(r + c)
-            neg_diagonal.add(r - c)
-            board[r] = [r, c]
-            solve_n_queens(r + 1)
-            # Backtrack
-            columns.remove(c)
-            pos_diagonal.remove(r + c)
-            neg_diagonal.remove(r - c)
-            board[r] = [0, 0]
+
+def init():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if sys.argv[1].isdigit():
+        the_queen = int(sys.argv[1])
+    else:
+        print("N must be a number")
+        sys.exit(1)
+    if the_queen < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    return(the_queen)
+
+
+def n_queens():
+
+    the_queen = init()
+    solver = solve(the_queen, the_queen)
+    for array in solver:
+        clean = []
+        for q, x in enumerate(array):
+            clean.append([q, x])
+        print(clean)
 
 
 if __name__ == '__main__':
-    solve_n_queens(0)
+    n_queens()
